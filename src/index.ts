292442a9679f43c709671935f131cdfdaea936e4
AbdelRahman Hamed <email4.m.rahman@gmail.com>
2021-11-22 00:59:36 +0200

    fix for hostname check

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const DEFAULT_PORT = 3000;
 const port = parseInt(process.env.PORT || '', 10) || DEFAULT_PORT;
 
 app.use('/health', (req, res) => res.send('ok'));
-app.use('/host', (req, res) => res.send(process.env.HOST));
+app.use('/hostname', (req, res) => res.send(process.env.HOSTNAME));
 app.use('/', (req, res) => res.send('🍪'));
 
 app.listen(port, () => console.log(`serving cookies from http://localhost:${port}/`));
