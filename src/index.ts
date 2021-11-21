0c2a2b8b170cf4d793ad2521305346b758e51926
AbdelRahman Hamed <email4.m.rahman@gmail.com>
2021-11-22 00:47:15 +0200

    add health check

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const app = express();
 const DEFAULT_PORT = 3000;
 const port = parseInt(process.env.PORT || '', 10) || DEFAULT_PORT;
 
+app.use('/health', (req, res) => res.send('ok'));
 app.use('/', (req, res) => res.send('🍪'));
 
 app.listen(port, () => console.log(`serving cookies from http://localhost:${port}/`));
