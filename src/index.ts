6104defa59ace37bbbe305b9cead6c40fd5ee5b3
AbdelRahman Hamed <email4.m.rahman@gmail.com>
2021-11-22 03:16:31 +0200

    routes

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ const app = express();
 const DEFAULT_PORT = 3000;
 const port = parseInt(process.env.PORT || '', 10) || DEFAULT_PORT;
 
-app.use('/health', (req, res) => res.send('ok'));
-app.use('/hostname', (req, res) => res.send(process.env.HOSTNAME));
-app.use('/users', (req, res) => res.send(`👩🏼‍🦰👨🏻‍⚕️🕵🏼‍♀️🧓🏻👱🏻‍♂️👦🏻 served from ${process.env.HOSTNAME}`));
-app.use('/apps', (req, res) => res.send(`👩🏻‍💻👨🏻‍💻🧑🏼‍💻🧑🏿‍💻👨🏼‍💻👩‍💻 served from ${process.env.HOSTNAME}`));
-app.use('/products', (req, res) => res.send(`🍩🍪🥧🥐🥯🥖 served from ${process.env.HOSTNAME}`));
-app.use('/', (req, res) => res.send(`🍪 served from ${process.env.HOSTNAME}`));
+app.get('/', (req, res) => res.send(`🍪 served from ${process.env.HOSTNAME}`));
+app.get('/health', (req, res) => res.send('ok'));
+app.get('/hostname', (req, res) => res.send(process.env.HOSTNAME));
+app.get('/users', (req, res) => res.send(`👩🏼‍🦰👨🏻‍⚕️🕵🏼‍♀️🧓🏻👱🏻‍♂️👦🏻 served from ${process.env.HOSTNAME}`));
+app.get('/apps', (req, res) => res.send(`👩🏻‍💻👨🏻‍💻🧑🏼‍💻🧑🏿‍💻👨🏼‍💻👩‍💻 served from ${process.env.HOSTNAME}`));
+app.get('/products', (req, res) => res.send(`🍩🍪🥧🥐🥯🥖 served from ${process.env.HOSTNAME}`));
+app.get('*', (req, res) => res.send('burned!'));
 
 app.listen(port, () => console.log(`serving cookies from http://localhost:${port}/`));
